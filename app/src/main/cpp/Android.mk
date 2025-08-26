562769d7fdd055d8d6ea789b4e3f1f9e1007581f
awkoo <184658409+awkoo@users.noreply.github.com>
2025-08-26 22:37:20 +0800

    Clean up codes
    Signed-off-by: awkoo <184658409+awkoo@users.noreply.github.com>

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -3,11 +3,5 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := terminal
 LOCAL_SRC_FILES := terminal.c
-LOCAL_CONLYFLAGS := -std=c11 -Wall -Wextra -Werror -Os -fno-stack-protector -Wl,--gc-sections
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_LDLIBS := -llog
-LOCAL_MODULE := local-socket
-LOCAL_SRC_FILES := local-socket.cpp
+LOCAL_CONLYFLAGS := -std=c11 -Wall -Wextra -Werror -O3 -fno-stack-protector -Wl,--gc-sections
 include $(BUILD_SHARED_LIBRARY)
